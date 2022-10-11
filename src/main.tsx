a057e62358aa5e60ce3d0306b37f3d67de60f83e
rootnick <rootnick1@gmail.com>
2022-10-11 15:35:21 +0300

    feat: add structure app (#1)

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
-import './index.css';
+import { App } from '@/components/pages/App';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container!);
